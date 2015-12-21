62279d96ce5c34e29e117cbf898cd9cbd296793d
OKharkovskyi <kharkovskyi.oleksandr@gmail.com>
2015-12-21 23:38:38 +0200

    Initial spring-boot project commit

---
+++ src/main/java/org/coursera/datastructures/AdvancedDatastructuresApplication.java
@@ -0,0 +1,12 @@
+package org.coursera.datastructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedDatastructuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedDatastructuresApplication.class, args);
+    }
+}
---
+++ src/test/java/org/coursera/datastructures/AdvancedDatastructuresApplicationTests.java
@@ -0,0 +1,18 @@
+package org.coursera.datastructures;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AdvancedDatastructuresApplication.class)
+@WebAppConfiguration
+public class AdvancedDatastructuresApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
